663bedc540883377e34c9d790f7e42db6e14d8b9
Anton Vogel <AntonVogel@ymail.com>
2020-05-01 10:26:30 +0200

    initial commit

---
+++ DeepWorkTimeTracker/MainMenu.cpp
@@ -0,0 +1,21 @@
+#include "MainMenu.h"
+
+MainMenu::MainMenu(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	SetupMenu();
+}
+
+void MainMenu::SetupMenu() {
+	m_centralWidget = this->centralWidget();
+	m_buttonLayout = new QVBoxLayout(m_centralWidget);
+	m_startTimerbutton = new QPushButton("Start Timer", m_centralWidget);
+	m_addTimebutton = new QPushButton("Add Time", m_centralWidget);
+	m_addSkillbutton = new QPushButton("Add Skill", m_centralWidget);
+	m_statsButton = new QPushButton("See Stats", m_centralWidget);
+	m_buttonLayout->addWidget(m_startTimerbutton);
+	m_buttonLayout->addWidget(m_addTimebutton);
+	m_buttonLayout->addWidget(m_addSkillbutton);
+	m_buttonLayout->addWidget(m_statsButton);
+}
---
+++ DeepWorkTimeTracker/MainMenu.h
@@ -0,0 +1,64 @@
+#pragma once
+/**
+ToDo List looks liek this
+
+projects (add new project, add subproject to existing project, browse projects
+
+Add a project
+"Would you like to ... add a new project"
+Add a
+--> the project should include
+
+LightMode/DarkMode
+Set Defalult mode
+
+You create a project ie research project X
+You can create tasks or tasklists(templates)
+you can schedule them
+set deadlines
+set estimated workload
+view statistics (dates up ahead, time put in / of X hours)
+
+Extras : Tomatoe timers (show how many tomatoe timers are left to go)
+Tomatoe mode (Tomatoe animation)
+
+Add in shortcuts
+(win + p)
+new project (n) browse projects (b)
+(win + t)
+new task (assign to project? assign all current tasks to project?)
+
+There should be a website to which all of this shit is being uploaded (WT)
+The website should give you the possibility to
+
+
+Mission Critical Mode
+A Tim Ferris inspired To-Do-List in which you can define two (more possible but not recommended) tasks that are absolutely essential for the progress of your life mission.
+Each task can be outfitted with sub tasks that require completion before the big task can be crossed off.
+
+
+*/
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/qpushbutton.h>
+#include <QtWidgets/qinputdialog.h>
+#include <QtWidgets/qboxlayout.h>
+#include <QtWidgets/qwidget.h>
+#include "ui_MainMenu.h"
+
+class MainMenu : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	MainMenu(QWidget *parent = Q_NULLPTR);
+
+private:
+	void SetupMenu();
+	Ui::MainMenuClass ui;
+	QVBoxLayout *m_buttonLayout = nullptr;
+	QPushButton *m_startTimerbutton = nullptr;
+	QPushButton *m_addSkillbutton = nullptr;
+	QPushButton *m_addTimebutton = nullptr;
+	QPushButton *m_statsButton = nullptr;
+	QWidget *m_centralWidget = nullptr;
+};
