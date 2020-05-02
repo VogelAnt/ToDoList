f3fb9aee89538b2d9e526b73f366844712356747
Anton Vogel <AntonVogel@ymail.com>
2020-05-02 17:34:14 +0200

    reworking aweful digital stopwatch implementation

--- DeepWorkTimeTracker/StopWatch.cpp
+++ DeepWorkTimeTracker/StopWatch.cpp
@@ -1,2 +1,33 @@
 #include "StopWatch.h"
 
+StopWatch::StopWatch(QWidget *parent = 0) : QWidget(parent) {
+
+}
+
+void StopWatch::on_StartTimer() {
+	{
+		mStartTime = QTime::currentTime();
+		mRunning = true;
+	}
+}
+
+void StopWatch::on_StopTimer() {
+		mRunning = false;
+}
+
+void StopWatch::timerEvent(QTimerEvent*) {
+	if (mRunning)
+	{
+		qint64 ms = mStartTime.msecsTo(QTime::currentTime());
+		int h = ms / 1000 / 60 / 60;
+		int m = (ms / 1000 / 60) - (h * 60);
+		int s = (ms / 1000) - (m * 60);
+		ms = ms - (s * 1000) - (m * 60000) - (h * 3600000);
+		QString diff = QString("%1:%2:%3:%4").
+			arg(h, 3, 10, zero).
+			arg(m, 2, 10, zero).
+			arg(s, 2, 10, zero).
+			arg(ms, 3, 10, zero);
+		mLabel->setText(diff);
+	}
+}
--- DeepWorkTimeTracker/StopWatch.h
+++ DeepWorkTimeTracker/StopWatch.h
@@ -13,18 +13,14 @@ class StopWatch : public QWidget
 {
 	Q_OBJECT
 public:
-	explicit StopWatch(QWidget * parent = 0)
-		: QWidget(parent)
-		, mRunning(false)
-		, mStartTime()
-		, mLabel(new QLabel("000:00:00:000"))
-		, zero('0')
-	{
+	explicit StopWatch(QWidget * parent = 0) : QWidget(parent), mRunning(false), mStartTime(), mLabel(new QLabel("000:00:00:000")), zero('0'){
+		// remove later on
 		QHBoxLayout * hBoxLayout = new QHBoxLayout(this);
 		QPushButton * startButton = new QPushButton("Start");
 		QPushButton * stopButton = new QPushButton("Stop");
 
-		QFont f("Arial", 32, QFont::Bold);
+		// set this in the constructor
+		QFont f("Arial", 22, QFont::Bold);
 		mLabel->setFont(f);
 
 		hBoxLayout->addWidget(startButton);
@@ -38,37 +34,18 @@ public:
 	}
 
 public slots:
-	void start(void)
-	{
-		mStartTime = QTime::currentTime();
-		mRunning = true;
-	}
-
-	void stop(void)
-	{
-		mRunning = false;
-	}
+	void on_StartTimer();
+	void on_StopTimer();
 
 protected:
-	void timerEvent(QTimerEvent *)
-	{
-		if (mRunning)
-		{
-			qint64 ms = mStartTime.msecsTo(QTime::currentTime());
-			int h = ms / 1000 / 60 / 60;
-			int m = (ms / 1000 / 60) - (h * 60);
-			int s = (ms / 1000) - (m * 60);
-			ms = ms - (s * 1000) - (m * 60000) - (h * 3600000);
-			QString diff = QString("%1:%2:%3:%4").
-				arg(h, 3, 10, zero).
-				arg(m, 2, 10, zero).
-				arg(s, 2, 10, zero).
-				arg(ms, 3, 10, zero);
-			mLabel->setText(diff);
-		}
-	}
+	void timerEvent(QTimerEvent *);
 
 private:
+	QVBoxLayout *m_vboxlayout = nullptr;
+	// QHBoxLayout *m_hlayout = nullptr;
+	QPushButton *m_startButton = nullptr;
+	QPushButton *m_stopButton = nullptr;
+
 	bool mRunning;
 	QTime mStartTime;
 	QLabel * mLabel;
