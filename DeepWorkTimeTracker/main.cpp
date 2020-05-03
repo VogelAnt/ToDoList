5e49e86cf805e040fb044ada38b8ad5b837564fe
Anton Vogel <AntonVogel@ymail.com>
2020-05-03 13:38:03 +0200

    sdfdfdfg

--- DeepWorkTimeTracker/main.cpp
+++ DeepWorkTimeTracker/main.cpp
@@ -3,11 +3,9 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/qdesktopwidget.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	QApplication a(argc, argv);
 	MainMenu w;
 	w.show();
-	//a.connect(&a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()));
 	return a.exec();
 }
