2a9a2a7d6f4118466c4cba84f5591f746568a2ff
Anton Vogel <AntonVogel@ymail.com>
2020-05-12 16:51:28 +0200

    Added map

--- DeepWorkTimeTracker/Skill.h
+++ DeepWorkTimeTracker/Skill.h
@@ -2,6 +2,7 @@
 #include <qtimer.h>
 #include <QtWidgets/qinputdialog.h>
 #include <qjsonarray.h>
+#include <qmap.h>
 #include <qdebug.h>
 #include <qfile.h>
 #include <qtextstream.h>
@@ -19,7 +20,7 @@ public:
 public slots:
 	void on_NewSkillAdded(QString newSkill);
 private:
-	QStringList m_skillList;
+	QMap<QString, int> *m_skillTimemap = nullptr;
 	QTimer *m_skillTimer = nullptr;
 	std::string m_skillName = "";
 	int m_skillTime = 0;
