03fb520110da26ae89b07cddad19c45266677336
Anton Vogel <AntonVogel@ymail.com>
2020-05-26 18:24:20 +0200

    Added class header for horizontal tab style

--- DeepWorkTimeTracker/SkillTab.h
+++ DeepWorkTimeTracker/SkillTab.h
@@ -2,6 +2,17 @@
 
 #include <QObject>
 #include <qtabwidget.h>
+#include <qproxystyle.h>
+#include <qwidget.h>
+#include <qstyle.h>
+#include <qsize.h>
+
+class HorizontalTabStyle : public QProxyStyle {
+public:
+	// TODO: Add constant override
+	QSize sizefromContents(ContentsType eType, const QStyleOption *eOption, const QSize &eSize, const QWidget *eWidget);
+	void drawControl(ControlElement eElement, const QStyleOption *eOption, QPainter *ePainter, const QWidget *eWidget);
+};
 
 class SkillTab : public QTabWidget
 {
@@ -10,4 +21,6 @@ class SkillTab : public QTabWidget
 public:
 	SkillTab(QObject *parent);
 	~SkillTab();
+private:
+	HorizontalTabStyle *m_tabStyle = nullptr;
 };
