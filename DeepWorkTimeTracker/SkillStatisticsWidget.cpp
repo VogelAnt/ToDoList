377e4e7396f74a8354c39a4484ad8b3f7a2f6ce4
Anton Vogel <AntonVogel@ymail.com>
2020-05-28 08:04:20 +0200

    Added SkillWidget Layout

--- DeepWorkTimeTracker/SkillStatisticsWidget.cpp
+++ DeepWorkTimeTracker/SkillStatisticsWidget.cpp
@@ -2,7 +2,11 @@
 
 SkillStatisticsWidget::SkillStatisticsWidget(QString skillName, int skillTime, QWidget *parent)
 	: QMainWindow(parent){
-
+	m_centralWidget = this->centralWidget();
+	m_verticalLayout = new QVBoxLayout(m_centralWidget);
+	QString labelString = skillName + ": " + QString::number(skillTime);
+	m_skillNametimelabel = new QLabel(labelString, m_centralWidget);
+	m_verticalLayout->addWidget(m_skillNametimelabel);
 }
 
 void SkillStatisticsWidget::on_TimeUpdated(QString skillName) {
--- DeepWorkTimeTracker/SkillStatisticsWidget.h
+++ DeepWorkTimeTracker/SkillStatisticsWidget.h
@@ -3,6 +3,8 @@
 #include <QObject>
 #include <qmainwindow.h>
 #include <qwidget.h>
+#include <qlayout.h>
+#include <qlabel.h>
 #include "Skill.h"
 
 class SkillStatisticsWidget : public QMainWindow
@@ -14,4 +16,8 @@ public:
 	~SkillStatisticsWidget();
 public slots:
 	void on_TimeUpdated(QString skillName);
+private:
+	QWidget *m_centralWidget = nullptr;
+	QVBoxLayout *m_verticalLayout = nullptr;
+	QLabel *m_skillNametimelabel = nullptr;
 };
