f18295c67fa3dd9f7d838f22348c135619766156
VogelAnt <AntonVogel@ymail.com>
2020-06-28 21:48:08 +0200

    Weird Bug in SkillOverViewWidget

--- ProductivityTracker/SkillOverviewWidget.cpp
+++ ProductivityTracker/SkillOverviewWidget.cpp
@@ -1,11 +1,15 @@
 #include "SkillOverviewWidget.h"
-
+#include <QDebug>
 //
 SkillOverviewWidget::SkillOverviewWidget(QWidget *parent) : QMainWindow(parent){
+    m_central = new QWidget();
     m_central = this->centralWidget();
+    m_addButton = new QPushButton("ADD", m_central);
+    SkillWidget *test = new SkillWidget("dsfdf", "fdfddfdf", "50", m_central);
     m_overViewlayout = new QVBoxLayout(m_central);
-    m_addButton = new QPushButton("ADD", this);
+    m_overViewlayout->addWidget(test);
     m_overViewlayout->addWidget(m_addButton);
+    qDebug() << "Added Widgets in SkillOverview" ;
 }
 
 // fetches data from SQL Client, Redis Client or File
